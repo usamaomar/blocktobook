7b2f6486935074d04a0f8ace901e9c176a430e3e
usamaomar <usamaomarsoftware@gmail.com>
2024-12-14 23:03:05 +0300

    c

--- src/main/kotlin/com/example/util/Constants.kt
+++ src/main/kotlin/com/example/util/Constants.kt
@@ -3,7 +3,8 @@ package com.example.util
 object Constants {
     const val AUDIENCE = "439843256699-o2oqqk1vm64qmi06fhl0ufsrs2blnmp8.apps.googleusercontent.com"
     const val ISSUER = "https//accounts.google.com"
-    const val DATABASE_NAME = "database"
+//    const val DATABASE_NAME = "database"
+    const val DATABASE_NAME = "cluster0.yew7v.mongodb.net"
     const val COMPANY_INFO_NOT_FOUND = 2
     const val COMPANY_INFO_NOT_VERIFIED = 29
     const val SUBSCRIPTION_NOT_FOUND = 4
