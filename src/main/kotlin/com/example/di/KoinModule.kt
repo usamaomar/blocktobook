db2cdaab1ad35c325e6c56325749b025fa3d155d
usamaomar <usamaomarsoftware@gmail.com>
2024-12-16 14:11:15 +0300

    c

--- src/main/kotlin/com/example/di/KoinModule.kt
+++ src/main/kotlin/com/example/di/KoinModule.kt
@@ -43,7 +43,7 @@ import org.litote.kmongo.reactivestreams.KMongo
 
 val koinModule = module {
     single {
-        KMongo.createClient(System.getenv("MONGODB_URI")).coroutine.getDatabase(DATABASE_NAME)
+        KMongo.createClient().coroutine.getDatabase(DATABASE_NAME)
     }
     single<UserDataSource> {
         UserDataSourceImpl(get())
