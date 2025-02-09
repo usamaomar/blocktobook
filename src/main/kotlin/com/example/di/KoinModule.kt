2d519c22de871995104c0adb384642b7efe5b196
usamaomar <usamaomarsoftware@gmail.com>
2025-02-09 14:42:55 +0300

    c

--- src/main/kotlin/com/example/di/KoinModule.kt
+++ src/main/kotlin/com/example/di/KoinModule.kt
@@ -49,9 +49,9 @@ val koinModule = module {
 
     single {
 //         if (System.getenv("APP_ENV") == "test") {
-            KMongo.createClient().coroutine.getDatabase(DATABASE_TEST) // Local test database
+//            KMongo.createClient().coroutine.getDatabase(DATABASE_TEST) // Local test database
 //        } else {
-//        KMongo.createClient(System.getenv("MONGODB_URI")).coroutine.getDatabase(DATABASE_NAME) // Production database
+        KMongo.createClient(System.getenv("MONGODB_URI")).coroutine.getDatabase(DATABASE_NAME) // Production database
 //        }
     }
 
